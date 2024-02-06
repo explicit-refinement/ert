7601e6e07b689fb86f23f099da83f533d1da77b5
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2024-02-06 11:08:29 +0000

    Fun with PERs

--- ERT/PER/Basic.lean
+++ ERT/PER/Basic.lean
@@ -1,9 +1,28 @@
 import Mathlib.Data.Set.Basic
 import Mathlib.Logic.Relator
+import Mathlib.Order.CompleteLattice
 
 structure PER {α} (r: α -> α -> Prop): Prop where
   symm: ∀{x y: α}, r x y -> r y x
   trans: ∀{x y z: α}, r x y -> r y z -> r x z
+
+def PER.eq {α}: PER (@Eq α) where
+  symm := Eq.symm
+  trans := Eq.trans
+
+def PER.univ {α}: @PER α ⊤ where
+  symm _ := True.intro
+  trans _ _ := True.intro
+def PER.empty {α}: @PER α ⊥ where
+  symm := id
+  trans _ := id
+
+def PER.inf {α} {r s: α -> α -> Prop} (R: PER r) (S: PER s): PER (r ⊓ s) where
+  symm H := ⟨R.symm H.left, S.symm H.right⟩
+  trans H H' := ⟨R.trans H.left H'.left, S.trans H.right H'.right⟩
+-- def PER.sInf {α: Type} {rs: Set (α -> α -> Prop)} (R: ∀r ∈ rs, PER r): PER (sInf r) where
+--   symm H p | ⟨Hp, Hp'⟩ => sorry
+--   trans H H' r Hr := sorry
 
 theorem PER.refl_left (A: PER r) (Hab: r a b): r a a
   := A.trans Hab (A.symm Hab)
@@ -101,3 +120,33 @@ def PSetoid.reflSetoid {α} (S: PSetoid α) (refl: ∀x, S.r x x)
 def Setoid.toPSetoid {α} (S: Setoid α): PSetoid α where
   r := S.r
   isper := Equivalence.toPER S.iseqv
+
+--TODO: clean this up...
+def Relation.onSet {α} (S: Set α) (r: α -> α -> Prop): α -> α -> Prop
+  := λa b => a ∈ S ∧ b ∈ S ∧ (r a b)
+
+def Relation.onSet.left {α} {S: Set α} {r} {a b}: onSet S r a b -> S a
+  | ⟨H, _, _⟩ => H
+def Relation.onSet.right {α} {S: Set α} {r} {a b}: onSet S r a b -> S b
+  | ⟨_, H, _⟩ => H
+def Relation.onSet.rel {α} {S: Set α} {r} {a b}: onSet S r a b -> r a b
+  | ⟨_, _, H⟩ => H
+
+def Relation.onSet_intersect {α} (S R: Set α) (r: α -> α -> Prop)
+  : onSet (S ∩ R) r = onSet S r ⊓ onSet R r
+  := by
+    funext a b
+    simp only [onSet, Set.mem_inter_iff, Pi.inf_apply, inf_Prop_eq, eq_iff_iff]
+    aesop
+def Relation.onSet_sup {α} (S: Set α) (r s: α -> α -> Prop)
+  : onSet S r ⊔ onSet S s = onSet S (r ⊔ s)
+  := by funext a b; simp only [Pi.sup_apply, onSet, sup_Prop_eq, eq_iff_iff]; aesop
+def Relation.onSet_inf {α} (S: Set α) (r s: α -> α -> Prop)
+  : onSet S r ⊓ onSet S s = onSet S (r ⊓ s)
+  := by funext a b; simp only [Pi.inf_apply, onSet, inf_Prop_eq, eq_iff_iff]; aesop
+
+--TODO: sInf, sSup, etc...
+
+def PER.onSet {α} {r: α -> α -> Prop} (R: PER r) (S: Set α): PER (Relation.onSet S r) where
+  symm H := ⟨H.right, H.left, R.symm H.rel⟩
+  trans H H' := ⟨H.left, H'.right, R.trans H.rel H'.rel⟩
